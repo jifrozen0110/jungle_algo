035d7d503cc1b189a21a8e0d716476c66e67c73d
최수빈 <lightsaber2929@gmail.com>
2024-08-10 15:51:25 +0900

    틀린 문제 수정

--- 최수빈/13_4344.py
+++ 최수빈/13_4344.py
@@ -3,25 +3,23 @@
 import sys
 
 input_num = int(sys.stdin.readline())
-input_val = []
 result_arr = []
 for i in range(input_num):
     line_val = list(map(int, sys.stdin.readline().split()))
-    input_val.append(line_val)
     avg = sum(line_val[1:]) / line_val[0]  # 평균 계산
-    print(avg)
 
     # 평균 넘는 인원수 구하기
     student = 0
-    for i in range(1, line_val[0]):
+    for score in line_val[1:]:
         # 평균과 value값 비교
-        if avg <= line_val[i]:
+        if score > avg: 
             student += 1
 
     # 비율 구하기
     ratio = student/line_val[0]*100
-    result_arr.append(str(round(ratio, 3)) + '%')
+    # result_arr.append(str(round(ratio, 3)) + '%')
+    result_arr.append(f"{ratio:.3f}%")
 
 print(*result_arr, sep='\n')
-        
+
 
